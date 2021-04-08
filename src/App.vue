dbcd7bb5684223637042b6837a0b6dea822ed58a
Anastasiya Kazanina <61517307+Solio69@users.noreply.github.com>
2021-04-08 16:14:05 +0300

    identity card

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <FBI></FBI>
+  </div>
+</template>
+
+<script>
+import FBI from "./components/FBI.vue";
+
+export default {
+  name: "App",
+  components: {
+    FBI,
+  },
+};
+</script>
+
+<style></style>
